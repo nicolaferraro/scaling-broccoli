02925b8f9cd3ddac362e9f49ab3150c45a133da6
Nicola Ferraro <ni.ferraro@gmail.com>
2017-06-04 15:33:05 +0200

    Fix test

--- broccoli-core/src/test/java/io/broccoli/builder/DatabaseSqlBuilderTest.java
+++ broccoli-core/src/test/java/io/broccoli/builder/DatabaseSqlBuilderTest.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.time.Duration;
 
 import io.broccoli.core.Database;
+import io.broccoli.core.Row;
 import io.broccoli.core.Table;
 import io.broccoli.util.TestEventFactory;
 import io.broccoli.versioning.VersioningSystem;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import javaslang.collection.List;
 import reactor.core.publisher.Flux;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author nicola
@@ -20,7 +23,6 @@ import reactor.core.publisher.Flux;
 public class DatabaseSqlBuilderTest {
 
     @Test
-    @Ignore("fix")
     public void testBuildSalesDatabase() throws IOException {
 
         DatabaseSqlBuilder builder = new DatabaseSqlBuilder();
@@ -38,14 +40,17 @@ public class DatabaseSqlBuilderTest {
 
         Table result = db.newQueryBuilder()
                 .select("description")
-                .from("product")
+                .from("products")
                 .buildQuery(v.current());
+
         result.changes().last().block(Duration.ofSeconds(5));
 
 
+        List<Row> rows = List.ofAll(result.stream(v.current()).collectList().block());
+        assertEquals(2, rows.size());
 
-
-
+        assertEquals("Apple iPhone 7", rows.get(0).cell(0));
+        assertEquals("OnePlus 3", rows.get(1).cell(0));
     }
 
 }
